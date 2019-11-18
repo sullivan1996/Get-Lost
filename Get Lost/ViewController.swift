83831b9885739ef9eca6f0a1b16f23ad052c1424
Sullivan De Carli <sullivan.dec69@gmail.com>
2019-11-18 16:52:06 +0100

    Adding authorize button

--- Get Lost/ViewController.swift
+++ Get Lost/ViewController.swift
@@ -1,21 +1,33 @@
-//
-//  ViewController.swift
-//  Get Lost
-//
-//  Created by Sullivan De carli on 18/11/2019.
-//  Copyright © 2019 Sullivan De carli. All rights reserved.
-//
-
+import HealthKit
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let button = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        
+        setupButton()
     }
-
-
+    
+    func setupButton() {
+        button.backgroundColor = .blue
+        button.setTitle("Authorize HealthKit", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 1
+        self.view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -20).isActive = true
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        //HealthKit access
+        print("Button tapped")
+    }
+    
+    
 }
 
